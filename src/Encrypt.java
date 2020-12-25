6c8f04377d5be713c9b628cfa0578c2752881255
Aviv Landesman <aviv13523@gmail.com>
2020-12-25 22:45:22 +0200

    Created project

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,2 +1,17 @@
+
+import java.io.*;
+import java.security.*;
+import javax.crypto.*;
+
 public class Encrypt {
+
+    public static void main(String args[]) {
+
+        String keyStorePassword = args[0];
+
+    }
+
+
+
+
 }
